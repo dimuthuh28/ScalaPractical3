6f65c1a92463c4d046a1f155ea176a0f09e270bc
Dimuthu Harshamal <78658133+dimuthuh28@users.noreply.github.com>
2024-07-02 10:29:22 +0530

    Update 3.scala

--- 3.scala
+++ 3.scala
@@ -1,12 +1,12 @@
 object AverageCalculator extends App {
 
   def calculateAverage(num1: Int, num2: Int): Double = {
-    val average = (num1 + num2).toDouble / 2
-    BigDecimal(average).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+    val average = (num1 + num2) / 2.00
+    average
   }
 
   val num1 = 10
-  val num2 = 20
+  val num2 = 21
   val average = calculateAverage(num1, num2)
-  println(average)
+  println(f"$average%.2f")
 }
